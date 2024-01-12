7aeb88e23d3f36575c0c12e679650df9d3d66197
clockgit <christopher.lock@gsa.gov>
2024-01-12 08:36:04 -0600

    reuse driver, default size, remove logging

--- test.py
+++ test.py
@@ -20,8 +20,11 @@ class WebsiteComparer:
     def __init__(self):
         self.ff_options = webdriver.FirefoxOptions()
         self.ff_options.add_argument("--headless")
+        self.ff_options.add_argument("--width=2560")
+        self.ff_options.add_argument("--height=1440")
         self.c_options = webdriver.ChromeOptions()
         self.c_options.add_argument("--headless")
+        self.c_options.add_argument("--window-size=2560,1440")
         self.mode = 0o755
         self.baseDir = './report/'
         self.checked_urls = {}
@@ -58,7 +61,6 @@ class WebsiteComparer:
             response = requests.head(url)
             if response.status_code not in [200, 301, 302, 403]:
                 broken_links.append(f"({response.status_code}) {url}")
-                self.log(f"Broken link for {url}: {response.status_code}: {url}")
         return broken_links
 
     def analyze_page_load_times(self, driver, url, dir, site, path):
@@ -66,7 +68,6 @@ class WebsiteComparer:
         load_event_end = driver.execute_script("return window.performance.timing.loadEventEnd")
         page_load_time = (load_event_end - navigation_start) / 1000
         self.checked_urls[path][site]['load_time'] = page_load_time
-        self.log(f"{url} Page load: {page_load_time:.2f} seconds", dir)
 
     def diff(self, param):
         diff = HtmlDiff()
@@ -76,20 +77,25 @@ class WebsiteComparer:
         with open(f"{param['dir']}/html_diff.html", "w", encoding="utf-8") as file:
             file.write(content_diff)
         file.close()
-        self.log(f"Created: {param['dir']}/content_diff.html", param['dir'])
 
-    def compare_page(self, url1, url2, browser, debug = False):
+    def setup(self, folder = './report/', browser = 'c', debug = False):
         self.debug = debug
+        self.baseDir = folder
         if browser == "f":
-            driver1 = webdriver.Firefox(options=self.ff_options)
-            driver2 = webdriver.Firefox(options=self.ff_options)
+            self.driver1 = webdriver.Firefox(options=self.ff_options)
+            self.driver2 = webdriver.Firefox(options=self.ff_options)
         elif browser == "c":
-            driver1 = webdriver.Chrome(options=self.c_options)
-            driver2 = webdriver.Chrome(options=self.c_options)
+            self.driver1 = webdriver.Chrome(options=self.c_options)
+            self.driver2 = webdriver.Chrome(options=self.c_options)
         else:
             print("Please select browser with c or f.")
             quit()
 
+    def shutdown(self):
+        self.driver1.quit()
+        self.driver2.quit()
+
+    def compare_page(self, url1, url2):
         s1 = self.check_url(url1)
         s2 = self.check_url(url2, s1)
 
@@ -97,54 +103,67 @@ class WebsiteComparer:
             "1st": s1,
             "2nd": s2,
             "base_dir": self.baseDir,
-            "driver1": driver1,
-            "driver2": driver2,
+            "driver1": self.driver1,
+            "driver2": self.driver2,
         }
 
-        if(param['1st']['exist']):
-            self.log(f"Skipping: {url1}", self.baseDir)
+        if(s1['exist']):
+            print('Duplicate path detected, skipping!')
         else:
             # Create dir structure.
             param['dir'] = os.path.join(self.baseDir, param['1st']['path'] )
             os.makedirs(param['dir'], mode = self.mode, exist_ok = True)
 
-            self.log(f"Starting param:\r\n{param}", self.baseDir)
-
             # Ensure the main-content element loaded
             try:
-                driver1.get(url1)
-                driver2.get(url2)
-                main_1 = WebDriverWait(driver1, 5).until(
-                    EC.presence_of_element_located((By.ID, "main-content"))
+                self.driver1.get(url1)
+                self.driver2.get(url2)
+                WebDriverWait(self.driver1, 10).until(
+                    EC.all_of(
+                        EC.presence_of_element_located((By.ID, "main-content")),
+                        EC.presence_of_element_located((By.ID, "ZN_0d171YnFuvpJle6")),
+                    )
                 )
-                main_2 = WebDriverWait(driver2, 5).until(
-                    EC.presence_of_element_located((By.ID, "main-content"))
+                WebDriverWait(self.driver2, 10).until(
+                    EC.all_of(
+                        EC.presence_of_element_located((By.ID, "main-content")),
+                        EC.presence_of_element_located((By.ID, "ZN_0d171YnFuvpJle6")),
+                    )
                 )
+                #WebDriverWait(self.driver1, 5).until(
+                #    EC.presence_of_element_located((By.ID, "main-content"))
+                #)
+                #WebDriverWait(self.driver2, 5).until(
+                #    EC.presence_of_element_located((By.ID, "main-content"))
+                #)
                 # Analyze page load times.
-                self.analyze_page_load_times(driver1, url1, param['dir'], 's1', param['1st']['path'])
-                self.analyze_page_load_times(driver2, url2, param['dir'], 's2', param['1st']['path'])
+                self.analyze_page_load_times(self.driver1, url1, param['dir'], 's1', param['1st']['path'])
+                self.analyze_page_load_times(self.driver2, url2, param['dir'], 's2', param['1st']['path'])
                 # Check for differences in page content
-#                 self.diff(param)
+                #self.diff(param)
                 # Check for visual differences using screenshots
                 self.image_diff(param, 'header')
                 self.image_diff(param, 'main')
                 self.image_diff(param, 'footer')
                 # Compare page URLs
-#                 site1_page_urls = self.get_page_urls(url1)
-#                 site2_page_urls = self.get_page_urls(url2)
-#                 self.save_page_urls(url1, site1_page_urls, f"{param['dir']}/site1_page_links.txt")
-#                 self.save_page_urls(url2, site2_page_urls, f"{param['dir']}/site2_page_links.txt")
-#                 site1_broken_links = self.check_links(site1_page_urls)
-#                 site2_broken_links = self.check_links(site2_page_urls)
-#                 self.save_page_urls(url1, site1_broken_links, f"{param['dir']}/site1_broken_links.txt")
-#                 self.save_page_urls(url2, site2_broken_links, f"{param['dir']}/site2_broken_links.txt")
+                #site1_page_urls = self.get_page_urls(url1)
+                #site2_page_urls = self.get_page_urls(url2)
+                #self.save_page_urls(url1, site1_page_urls, f"{param['dir']}/site1_page_links.txt")
+                #self.save_page_urls(url2, site2_page_urls, f"{param['dir']}/site2_page_links.txt")
+                #site1_broken_links = self.check_links(site1_page_urls)
+                #site2_broken_links = self.check_links(site2_page_urls)
+                #self.save_page_urls(url1, site1_broken_links, f"{param['dir']}/site1_broken_links.txt")
+                #self.save_page_urls(url2, site2_broken_links, f"{param['dir']}/site2_broken_links.txt")
                 self.create_reports(self.checked_urls[param['1st']['path']])
 
             except Exception as e:
-                print(f'Exception: {e}')
-            finally:
-                driver1.quit()
-                driver2.quit()
+                if hasattr(e, 'message'):
+                    print(e)
+                    print(e.message)
+                else:
+                    print(e)
+            #finally:
+                #print(f'checked {url1} {url2}')
 
     def check_url(self, url, s1 = False):
         r = {"exist": True, "url": url}
@@ -164,17 +183,16 @@ class WebsiteComparer:
         else:
             r["page"] = 'home'
         if r["path"] in self.checked_urls.keys():
-          r["exist"] = True
+            r["exist"] = True
         else:
-          r["exist"] = False
-          if s1 == False:
+            r["exist"] = False
+        if s1 == False:
             self.checked_urls[r["path"]] = {'s1': {'site': r["site"], 'page': r["page"], 'path': r["path"], 'url': url, 'image':{}}, 's2':{}, 'diff':{}}
-          else:
+        else:
             self.checked_urls[s1['path']]['s2'] = {'site': r["site"], 'page': r["page"], 'path': r["path"], 'url': url, 'image':{}, 'diff':{}}
         return r
 
     def take_screenshot(self, driver, path, name, tag = 'main'):
-        self.log(f"take_screenshot(self, {driver}, {path}, {name})")
         img_url = f"{path}/{name}"
         ######
         #original_size = driver.get_window_size()
@@ -191,7 +209,6 @@ class WebsiteComparer:
         return Image.open(img_url).convert('RGB')
 
     def image_diff(self, param, tag='main'):
-        self.log(f"image_diff(self, {param})", self.baseDir)
         #time.sleep(5)
         image1 = self.take_screenshot(param['driver1'], param['dir'], tag + '_site_1.png', tag)
         image2 = self.take_screenshot(param['driver2'], param['dir'], tag + '_site_2.png', tag)
@@ -201,36 +218,25 @@ class WebsiteComparer:
         msg = 'yes'
         if diff_image.getbbox():
             msg = 'no'
-            print(f"Visual differences saved to {visual_diff}")
-        else:
-            print("No visual differences.")
+            #print(f"Visual differences saved to {visual_diff}")
+        #else:
+            #print("No visual differences.")
+
         self.checked_urls[param['1st']['path']]['diff'][tag] = {'match': msg, 'image': visual_diff}
         self.checked_urls[param['1st']['path']]['s1']['image'][tag] = f"{param['dir']}/{tag}_site_1.png"
         self.checked_urls[param['1st']['path']]['s2']['image'][tag] = f"{param['dir']}/{tag}_site_2.png"
 
-
-
-    def log(self, msg, dir = './report/'):
-        if self.debug:
-            print(f"Debug:\r\n{msg}\r\n")
-        if(dir != self.baseDir):
-            f = open(dir + '/page.log', 'a')
-            f.write(f"{msg}\r\n")
-            f.close()
-        f = open('report/main.log', 'a')
-        f.write(f"{msg}\r\n")
-        f.close()
 
     def save_page_urls(self, url, page_urls, filename):
         with open(filename, "w") as file:
             file.write(f"Page URLs for {url}:\n")
             for page_url in page_urls:
                 file.write(page_url + "\n")
-        self.log(f"URLs saved to {filename}.")
     def create_report_start(self, d1, d2):
         start = "<head><style>.space{padding:5px 0;} .path{width:4vw;} table{width:100%} td{padding:10px;} .site{width:25%} .dif{width:75%;} img{width: 100%; height: auto;} tr:nth-child(2n+2) {background-color: #FCC;} tr:nth-child(2n+3) {background-color: #CFC;}</style>\r\n<body>\r\n<h1>" + f'd1: {d1}<br/>d2: {d2}' + "</h1>\r\n<table>\r\n"
-        with open('./report/report.html', "w") as file:
-                file.write(f'<a href="images.html">image</a>\r\n')
+        with open(f'{self.baseDir}report.html', "w") as file:
+                for tag in self.tags:
+                    file.write(f'<a class="space" href="{tag}.html">{tag}</a>\r\n')
                 file.write(start)
 
                 file.write("<tr>\r\n")
@@ -241,26 +247,28 @@ class WebsiteComparer:
                 for tag in self.tags:
                     file.write(f'<th class="image">d1 {tag}</th>')
                     file.write(f'<th class="image">d2 {tag}</th>')
-                    file.write("<th>Match</th>")
                     file.write(f'<th class="image">Diff {tag}</th>\r\n')
                 file.write("</tr>\r\n")
-        with open('./report/images.html', "w") as file:
-                file.write(f'<a href="report.html">main report</a>\r\n')
-                file.write(start)
+        for tag in self.tags:
+            with open(f'{self.baseDir}{tag}.html', "w") as file:
+                    file.write(f'<a href="report.html">main report</a>\r\n')
+                    file.write(f'<h1>{tag}</h1>')
+                    file.write(start)
 
-                file.write("<tr>\r\n")
-                file.write('<th class="site">Site</th>')
-                file.write('<th class="diff">Diff</th>\r\n')
-                file.write("</tr>\r\n")
+                    file.write("<tr>\r\n")
+                    file.write('<th class="site">Site</th>')
+                    file.write('<th class="diff">Diff</th>\r\n')
+                    file.write("</tr>\r\n")
     def create_report_end(self):
         end = "</table>\r\n</body>\r\n"
-        with open('./report/report.html', "a") as file:
+        with open(f'{self.baseDir}report.html', "a") as file:
             file.write(end)
-        with open('./report/images.html', "a") as file:
-            file.write(end)
+        for tag in self.tags:
+            with open(f'{self.baseDir}{tag}.html', "a") as file:
+                file.write(end)
 
     def create_reports(self, data):
-        with open('./report/report.html', "a") as file:
+        with open(f'{self.baseDir}report.html', "a") as file:
             file.write(f'<tr>\r\n')
             #file.write(f"<td>{data['s1']['site']}</td>")
             file.write(f"<td>{data['s1']['page']}</td>")
@@ -268,18 +276,21 @@ class WebsiteComparer:
             file.write(f'<td>{data['s1']['load_time']} / {data['s2']['load_time']}</td>\r\n')
 
             for tag in self.tags:
-                img = data['s1']['image'][tag].replace("./report/", "").replace("//", "/")
+                img = data['s1']['image'][tag].replace(self.baseDir, "").replace("//", "/")
                 file.write(f'<td><a href="{img}" target="_blank"><img src="{img}"></a></td>\r\n')
-                img = data['s2']['image'][tag].replace("./report/", "").replace("//", "/")
+                img = data['s2']['image'][tag].replace(self.baseDir, "").replace("//", "/")
                 file.write(f'<td><a href="{img}" target="_blank"><img src="{img}"></a></td>\r\n')
 
-                file.write(f"<td>{data['diff'][tag]['match']}</td>")
-                img = data["diff"][tag]['image'].replace("./report/", "").replace("//", "/")
-                file.write(f'<td><a href="{img}" target="_blank"><img src="{img}"></a></td>\r\n')
+                if data["diff"][tag]['match'] == 'no':
+                    img = data["diff"][tag]['image'].replace(self.baseDir, "").replace("//", "/")
+                    file.write(f'<td><a href="{img}" target="_blank"><img src="{img}"></a></td>\r\n')
+                else:
+                    file.write(f'<td>Match</td>\r\n')
+
             file.write("</tr>\r\n")
 
-        with open('./report/images.html', "a") as file:
-            for tag in self.tags:
+        for tag in self.tags:
+            with open(f'{self.baseDir}{tag}.html', "a") as file:
                 file.write(f'<tr>\r\n')
                 file.write('<td class="site">')
                 file.write('<h2 class="space">')
@@ -289,28 +300,37 @@ class WebsiteComparer:
                 file.write(f'<a title="{data['s1']['url']}" href="{data['s1']['url']}" target="_blank">S1</a><br />')
                 file.write(f'Load: {data['s1']['load_time']}<br />')
                 file.write(f'Page section: {tag}')
-                img1 = data['s1']['image'][tag].replace("./report/", "").replace("//", "/")
+                img1 = data['s1']['image'][tag].replace(self.baseDir, "").replace("//", "/")
                 file.write(f'<a href="{img1}" target="_blank"><img src="{img1}"></a>')
                 file.write('</div>')
 
                 file.write('<div class="space">')
                 file.write(f'<a title="{data['s2']['url']}" href="{data['s2']['url']}" target="_blank">S2</a><br />')
                 file.write(f'Load: {data['s2']['load_time']}<br />')
-                img2 = data['s2']['image'][tag].replace("./report/", "").replace("//", "/")
+                img2 = data['s2']['image'][tag].replace(self.baseDir, "").replace("//", "/")
                 file.write(f'<a href="{img2}" target="_blank"><img src="{img2}"></a>')
                 file.write('</div>')
 
                 file.write('</td>')
-                diff = data["diff"][tag]["image"].replace("./report/", "").replace("//", "/")
-                file.write(f'<td class="diff"><a href="{diff}" target="_blank"><img src="{diff}" width="33vw"></a></td>\r\n')
-                file.write("</tr>\r\n")
+                file.write(f'<td class="diff">')
+                if data["diff"][tag]['match'] == 'no':
+                    diff = data["diff"][tag]["image"].replace(self.baseDir, "").replace("//", "/")
+                    file.write(f'<a href="{diff}" target="_blank"><img src="{diff}" width="33vw"></a>')
+                else:
+                    file.write('Pixel perfect match!')
+                file.write("</td>\r\n</tr>\r\n")
 
 
 if __name__ == "__main__":
     comparer = WebsiteComparer()
     browser = click.prompt("(c)hrome or (f)ireFox", default="f")
     d1 = click.prompt("Enter the first domain:", default="https://www.gsa.gov")
+    #d2 = click.prompt("Enter the first domain:", default="https://www.gsa.gov")
     d2 = click.prompt("Enter the second domain:", default="https://gsa-test-d10.gsa-ecas.cloud")
+    folder = click.prompt("Enter folder name:", default="report")
+    os.makedirs(f'./{folder}/', mode = 0o755, exist_ok = True)
+
+    comparer.setup(folder = f'./{folder}/', browser=browser, debug=False)
 
     comparer.create_report_start(d1, d2)
 
@@ -324,9 +344,10 @@ if __name__ == "__main__":
             p2 = f"{d2}{line}".replace("\n", "").replace("\r", "")
             print(f"Comparing ({x} of {count}):\n    {p1}\n    {p2}")
             comparer.compare_page(
-                f"{p1}",
-                f"{p2}",
-                browser
+                p1,
+                p2
             )
+            #break
 
     comparer.create_report_end()
+    comparer.shutdown()
